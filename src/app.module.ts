c720040c40756fcac38e3afc4fcd57a089b96474
DrozhzhinD <drozhzhinxstudent@gmail.com>
2024-02-06 12:31:52 +0300

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PromoModule } from './promo/promo.module';
 
 @Module({
-  imports: [],
+  imports: [PromoModule],
   controllers: [AppController],
   providers: [AppService],
 })
